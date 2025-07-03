3d7b8a4dba0e283ceaa2560718d7aca9d48a3a0e
bensidney ndungu <sidneyvonex@gmail.com>
2025-07-03 16:34:07 +0300

    refactor: update package.json and pnpm-lock.yaml for improved dependencies and add devDependencies; enhance server.ts with express, dotenv, and cors integration

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-import express from "express"
+import express,{Application,Response} from "express";
+import dotenv from "dotenv"
+import cors from "cors"
+
+dotenv.config();
+
+const app:Application = express()
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/',(req,res:Response) =>{
+    res.send("Welcome to our Mpesa Integration Api using Node Js and Express Js")
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`🌟🌟Server is running on http://localhost:${PORT}`)
+})
+
