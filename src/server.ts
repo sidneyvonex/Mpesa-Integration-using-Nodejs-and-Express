cb671f4705e3296d68c9aef0380d67714d9c3f89
bensidney ndungu <sidneyvonex@gmail.com>
2025-07-04 05:46:09 +0300

    refactor: update server.ts to include error handling in STK endpoint and adjust default port

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,38 @@
-import express,{Application,Response} from "express";
+import express,{Application,Request,Response} from "express";
 import dotenv from "dotenv"
 import cors from "cors"
 
 dotenv.config();
 
 const app:Application = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+//Default Route
 app.get('/',(req,res:Response) =>{
     res.send("Welcome to our Mpesa Integration Api using Node Js and Express Js")
 })
 
+//initiate stk endpoint
+app.post('/stk',(req:Request,res:Response)=> {
+    // console.log("STK Endpoint is Working")
+    
+    //Add a error handler
+    try{
+        //Safaricom Phone Number(2547......), amount(num),productName("")
+        const {phoneNumber,amount,productName} = req.body;
+
+        //Initiate STK PUSH
+        //1.Authorization
+    }catch(error:any){
+        res.status(500).json({error:error.message})
+    }
+
+
+})
 
 app.listen(PORT,()=>{
     console.log(`🌟🌟Server is running on http://localhost:${PORT}`)
